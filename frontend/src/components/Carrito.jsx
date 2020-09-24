2d739eb05167b7fadd81d8a2966e234ab00be289
Celes-te <isaflower1008@gmail.com>
2020-09-23 20:16:51 -0500

    se agregó la pagina de carrito

---
+++ frontend/src/components/Carrito.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Carrito(){
+  return ( <div>
+
+
+
+
+
+  {/*Esto es un comentario className, className = "container", style = {{backgroundColor: "#000000"}}*/}    
+  </div>
+
+
+  );
+
+
+}
