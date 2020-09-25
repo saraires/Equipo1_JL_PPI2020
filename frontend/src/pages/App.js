b4e72ddd8f7997b8dd8d1ac374329dd8daf6cdc4
Valentinaduess <valentinaduess@gmail.com>
2020-09-24 20:33:36 -0500

    Se realizó una página de Inicio de sesión

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -4,14 +4,16 @@ import Config from '../components/Config';
 import Historial from '../components/Historial';
 import MenuPremios from '../components/MenuPremios';
 import styles from '../components/styles.css';
-
+import Formulario from './Pages/Formulario';
+import Inicio_sesion from './Pages/Inicio_sesion';
+import Premios from './Pages/Premios';
 
 function App() {
   return (
     <div className="App">
-      <MenuPremios/>
-      <Historial/>
-      </div>
+      <MenuPremios />
+      <Historial />
+    </div>
   );
 }
 
