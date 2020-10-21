c5c584fb75af2ba21dd8787747e81df6ae640a81
saraires <sarairestrepo22@gmail.com>
2020-10-21 18:15:20 -0500

    Re-Deploy

--- frontend/src/pages/Introduccion.js
+++ frontend/src/pages/Introduccion.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../styles/stylesintro.css"
+import '../styles/stylesintro.css'
 
 import Carrucel from '../components/Carrucel';
 
